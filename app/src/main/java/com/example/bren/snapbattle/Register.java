dcbfdcb06148b07642e16369eb66000d8525c1a2
Bren <x13342426@student.ncirl.ie>
2018-11-09 14:49:34 +0000

    Firebase connected and working

--- app/src/main/java/com/example/bren/snapbattle/Register.java
+++ app/src/main/java/com/example/bren/snapbattle/Register.java
@@ -1,10 +1,13 @@
 package com.example.bren.snapbattle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,6 +17,10 @@ import com.google.firebase.auth.FirebaseAuthException;
 
 public class Register extends MainActivity implements View.OnClickListener{
     private FirebaseAuth mAuth;
+    private EditText editTextEmail;
+    private EditText editTextPass;
+    private TextView textViewSignup;
+    private Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,44 +30,53 @@ public class Register extends MainActivity implements View.OnClickListener{
         mAuth = FirebaseAuth.getInstance();
         findViewById(R.id.Blogin).setOnClickListener(this);
 
+
+        buttonRegister = (Button) findViewById(R.id.Blogin);
+        editTextEmail = (EditText) findViewById(R.id.tEmail);
+        editTextPass = (EditText) findViewById(R.id.tPass);
+        textViewSignup = (TextView) findViewById(R.id.tSign);
+
+        buttonRegister.setOnClickListener(this);
+        textViewSignup.setOnClickListener(this);
+
     }
 
 
-    public void onregisterClick(View v) {
-            EditText name = (EditText) findViewById(R.id.tUsn);
-            EditText email = (EditText) findViewById(R.id.tEmail);
-            EditText pass = (EditText) findViewById(R.id.tPass);
-            String namestr = name.getText().toString();
-            String emailstr = email.getText().toString();
-            String passstr = pass.getText().toString();
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String pass = editTextPass.getText().toString().trim();
 
-            if (emailstr.isEmpty()) {
-                email.setError("Email is required");
-                email.requestFocus();
+
+
+
+
+            if (email.isEmpty()) {
+                editTextEmail.setError("Email is required");
+                editTextEmail.requestFocus();
                 return;
             }
 
-            if (!Patterns.EMAIL_ADDRESS.matcher(emailstr).matches()) {
-                email.setError("Please enter a valid email");
-                email.requestFocus();
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                editTextEmail.setError("Please enter a valid email");
+                editTextEmail.requestFocus();
                 return;
             }
 
-            if (passstr.isEmpty()) {
-                pass.setError("Password is required");
-                pass.requestFocus();
+            if (pass.isEmpty()) {
+                editTextPass.setError("Password is required");
+                editTextPass.requestFocus();
                 return;
 
             }
 
-            if (passstr.length() < 6) {
-                pass.setError("Password needs to be 6 characters");
-                pass.requestFocus();
+            if (pass.length() < 6) {
+                editTextPass.setError("Password needs to be 6 characters");
+                editTextPass.requestFocus();
                 return;
             }
 
 
-            mAuth.createUserWithEmailAndPassword(emailstr, passstr).addOnCompleteListener(new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
+            mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
                     if(task.isSuccessful()){
@@ -72,8 +88,19 @@ public class Register extends MainActivity implements View.OnClickListener{
 
 
 
+
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.Blogin:
+                registerUser();
+                break;
+
+            case R.id.tSign:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
 
     }
 }
